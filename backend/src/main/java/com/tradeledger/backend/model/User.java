466082dafb4548a71b0659667fadfba3f45a9327
Krushal Patel <krushal259@gmail.com>
2026-04-19 22:39:53 +0530

    Add User class for storing login information

---
+++ backend/src/main/java/com/tradeledger/backend/model/User.java
@@ -0,0 +1,21 @@
+package com.tradeledger.backend.model;
+
+// User class stores login information
+public class User {
+
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
