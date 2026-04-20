4e912fe5a2ddf23856480db14212746de21b4097
Krushal Patel <krushal259@gmail.com>
2026-04-20 21:32:00 +0530

    Refactor StockController to manage stock APIs

--- backend/src/main/java/com/tradeledger/backend/controller/StockController.java
+++ backend/src/main/java/com/tradeledger/backend/controller/StockController.java
@@ -1,13 +1,38 @@
 package com.tradeledger.backend.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.tradeledger.backend.model.Stock;
+import com.tradeledger.backend.service.StockService;
+
+// Using Controller concept to create REST APIs for frontend communication
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/stocks")
 public class StockController {
 
-    @GetMapping("/api/test")
-    public String test() {
-        return "Backend Connected Successfully!";
+    @Autowired
+    private StockService stockService;
+
+    // API to get all stocks
+    @GetMapping
+    public List<Stock> getAllStocks() {
+        return stockService.getAllStocks();
+    }
+
+    // API to get single stock by name
+    @GetMapping("/{name}")
+    public Stock getStock(@PathVariable String name) {
+        return stockService.getStockByName(name);
+    }
+
+    // API to update prices manually
+    @GetMapping("/update")
+    public String updatePrices() {
+        stockService.updatePrices();
+        return "Stock prices updated successfully";
     }
 }
