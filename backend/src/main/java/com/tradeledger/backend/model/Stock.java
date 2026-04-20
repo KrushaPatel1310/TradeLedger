92e8bf18bd94afa24842b4915d764c4548ea89bd
Krushal Patel <krushal259@gmail.com>
2026-04-20 22:32:03 +0530

    Update Stock.java

--- backend/src/main/java/com/tradeledger/backend/model/Stock.java
+++ backend/src/main/java/com/tradeledger/backend/model/Stock.java
@@ -1,16 +1,24 @@
 package com.tradeledger.backend.model;
 
-// Using OOP Encapsulation concept to protect stock data using private fields
+import java.util.Random;
+
+// Encapsulation for stock data
 public class Stock {
 
     private String name;
     private double price;
     private double change;
+    private String volume;
+    private String marketCap;
 
     public Stock(String name, double price, double change) {
         this.name = name;
         this.price = price;
         this.change = change;
+
+        // Auto generate values
+        this.volume = (100 + new Random().nextInt(900)) + "K";
+        this.marketCap = "₹" + (10 + new Random().nextInt(90)) + "K Cr";
     }
 
     public String getName() {
@@ -23,6 +31,14 @@ public class Stock {
 
     public double getChange() {
         return change;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public String getMarketCap() {
+        return marketCap;
     }
 
     public void setPrice(double price) {
