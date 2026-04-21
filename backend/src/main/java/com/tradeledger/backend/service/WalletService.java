8a7ea0a0fe47b293d6ee5e5ef90c94b13dc70ed4
Krushal Patel <krushal259@gmail.com>
2026-04-21 18:31:31 +0530

    Refactor WalletService to use balance management
    Refactored WalletService to manage balance directly instead of using Wallet model.

--- backend/src/main/java/com/tradeledger/backend/service/WalletService.java
+++ backend/src/main/java/com/tradeledger/backend/service/WalletService.java
@@ -1,23 +1,28 @@
 package com.tradeledger.backend.service;
 
 import org.springframework.stereotype.Service;
-import com.tradeledger.backend.model.Wallet;
 
-// Service layer handles wallet logic
+// Using Service layer and Encapsulation concept to manage wallet balance securely
 @Service
 public class WalletService {
 
-    private Wallet wallet = new Wallet();
+    private double balance = 20000;
 
     public double getBalance() {
-        return wallet.getBalance();
+        return balance;
     }
 
-    public void deductMoney(double amount) {
-        wallet.deduct(amount);
+    public boolean deduct(double amount) {
+
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+
+        return false;
     }
 
-    public void addMoney(double amount) {
-        wallet.add(amount);
+    public void add(double amount) {
+        balance += amount;
     }
 }
