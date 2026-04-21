ca963ad5e2739003967030a22228f8c14235ecd3
Krushal Patel <krushal259@gmail.com>
2026-04-21 18:32:40 +0530

    Update PortfolioService.java

--- backend/src/main/java/com/tradeledger/backend/service/PortfolioService.java
+++ backend/src/main/java/com/tradeledger/backend/service/PortfolioService.java
@@ -4,38 +4,41 @@ import java.util.*;
 import org.springframework.stereotype.Service;
 import com.tradeledger.backend.model.PortfolioItem;
 
-// Manages portfolio stocks
+// Using Service layer and Collection Framework concept to manage portfolio stocks
 @Service
 public class PortfolioService {
 
-    private List<PortfolioItem> portfolio = new ArrayList<>();
+    private List<PortfolioItem> items = new ArrayList<>();
 
-    public List<PortfolioItem> getPortfolio() {
-        return portfolio;
+    public List<PortfolioItem> getAll() {
+        return items;
     }
 
-    public void buyStock(String name, int qty, double price) {
+    public void buy(String name, int qty, double price) {
 
-        for (PortfolioItem item : portfolio) {
+        for (PortfolioItem item : items) {
+
             if (item.getName().equalsIgnoreCase(name)) {
                 item.addQty(qty);
                 return;
             }
         }
 
-        portfolio.add(new PortfolioItem(name, qty, price));
+        items.add(new PortfolioItem(name, qty, price));
     }
 
-    public boolean sellStock(String name, int qty) {
+    public boolean sell(String name, int qty) {
 
-        for (PortfolioItem item : portfolio) {
+        for (PortfolioItem item : items) {
+
             if (item.getName().equalsIgnoreCase(name)) {
 
                 if (item.getQuantity() >= qty) {
+
                     item.reduceQty(qty);
 
                     if (item.getQuantity() == 0) {
-                        portfolio.remove(item);
+                        items.remove(item);
                     }
 
                     return true;
