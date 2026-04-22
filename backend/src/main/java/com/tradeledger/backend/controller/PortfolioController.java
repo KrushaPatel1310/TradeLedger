8fc08ab058bbf2e337343930498da39bd5671670
Krushal Patel <krushal259@gmail.com>
2026-04-22 12:10:17 +0530

    Update PortfolioController.java

--- backend/src/main/java/com/tradeledger/backend/controller/PortfolioController.java
+++ backend/src/main/java/com/tradeledger/backend/controller/PortfolioController.java
@@ -1,10 +1,14 @@
 package com.tradeledger.backend.controller;
 
-import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.tradeledger.backend.service.PortfolioService;
 
-// Using Controller concept to provide portfolio data to frontend
 @RestController
 @RequestMapping("/api/portfolio")
 @CrossOrigin(origins = "http://localhost:3000")
@@ -16,5 +20,25 @@ public class PortfolioController {
     @GetMapping
     public Object getPortfolio() {
         return portfolioService.getAll();
+    }
+
+    @GetMapping("/buy")
+    public String buyStock(
+            @RequestParam String name,
+            @RequestParam int qty,
+            @RequestParam double price) {
+
+        return portfolioService.buyStock(
+                name, qty, price);
+    }
+
+    @GetMapping("/sell")
+    public String sellStock(
+            @RequestParam String name,
+            @RequestParam int qty,
+            @RequestParam double price) {
+
+        return portfolioService.sellStock(
+                name, qty, price);
     }
 }
